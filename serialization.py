c98558d320db48261157f716352947c828a4e2d1
Akshat Talwar <uniquedimension1@gmail.com>
2025-12-08 00:41:25 +0530

    feat: add Address and Patient models with example instantiation

---
+++ serialization.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+class Patient(BaseModel):
+
+    name: str
+    gender: str = 'Male'
+    age: int
+    address: Address
+
+address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {'name': 'nitish', 'age': 35, 'address': address1}
+
+patient1 = Patient(**patient_dict)
+
+temp = patient1.model_dump(exclude_unset=True) # only include fields that were explicitly set means exclude default values which means gender
+print(temp)
+print(type(temp))
